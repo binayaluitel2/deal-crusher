e1ea8d9e7e5b7003b6864ced03e3ac57a331de73
binayaluitel2 <binayababuluitel@gmail.com>
2021-04-17 15:01:54 -0400

    Adding login and signup forms

--- pages/create.js
+++ pages/create.js
@@ -53,13 +53,12 @@ function CreateProduct() {
     return mediaUrl;
   }
 
-  
   async function handleSubmit(event) {
     try {
       event.preventDefault();
       setLoading(true);
+      setError("");
       const mediaUrl = await handleImageUpload();
-      console.log({ mediaUrl });
       const url = `${baseUrl}/api/product`;
       const { name, price, description } = product;
       const payload = { name, price, description, mediaUrl };
